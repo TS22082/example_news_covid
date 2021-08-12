ff6ee6907383833601ba525f7dcc1737fd53138f
TS22082 <ts22082@gmail.com>
2021-08-12 14:36:48 -0700

    update state render

--- src/App.js
+++ src/App.js
@@ -6,26 +6,28 @@ import Card from "./Card/Card";
 
 function App() {
   const [articles, setArticles] = useState([]);
+
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
-    (async () => {
-      try {
-        const res = await axios.get(
-          "https://newsapi.org/v2/top-headlines?country=us&apiKey=84145fe9236e4573824511f495ab8126"
-        );
-        setArticles(res.data.articles);
-      } catch (err) {
-        console.log(err);
-      }
-    })();
+    console.log("a render happened");
+  }, [count]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_KEY}`
+      )
+      .then((res) => setArticles(res.data.articles))
+      .catch((err) => console.log(err));
   }, []);
 
   useEffect(() => {
     fetch(
       "https://covid-19-data.p.rapidapi.com/report/country/name?name=Italy&date=2020-04-01",
       {
-        method: "GET",
         headers: {
-          "x-rapidapi-key": process.env.REACT_APP_KEY,
+          "x-rapidapi-key": process.env.REACT_APP_COVID_KEY,
           "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
         },
       }
@@ -39,6 +41,8 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Count: {count} </h1>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
       {articles.map((article, index) => (
         <Card
           key={index}
