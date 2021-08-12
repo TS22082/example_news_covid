9ae038839287f9bb3fdb05d9fdfb0c1ad0dfbb86
TS22082 <ts22082@gmail.com>
2021-08-12 14:08:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./Card/Card";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=us&apiKey=84145fe9236e4573824511f495ab8126"
+        );
+        setArticles(res.data.articles);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    fetch(
+      "https://covid-19-data.p.rapidapi.com/report/country/name?name=Italy&date=2020-04-01",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key": process.env.REACT_APP_KEY,
+          "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {articles.map((article, index) => (
+        <Card
+          key={index}
+          title={article.title}
+          description={article.description}
+        />
+      ))}
     </div>
   );
 }
